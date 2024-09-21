5dabc8b20d1814eac22dd31e487349ea5c5f5a04
Fred Glass <glasshcs@gmail.com>
2024-09-21 10:07:40 +0100

    Fix database initialisation

--- App.tsx
+++ App.tsx
@@ -1,17 +1,22 @@
+import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RootSiblingParent } from "react-native-root-siblings";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { initDatabase } from "./data-access/database";
 import HomeView from "./components/HomeView";
 
-// Run during module initialisation, before view renders
-if (typeof window !== "undefined") {
-  initDatabase();
-}
-
 const queryClient = new QueryClient();
 
 export default function App() {
+  useEffect(() => {
+    const init = async () => {
+      if (await initDatabase()) {
+        queryClient.invalidateQueries();
+      }
+    };
+    init();
+  }, []);
+
   return (
     <QueryClientProvider client={queryClient}>
       <RootSiblingParent>
